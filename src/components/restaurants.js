471901c4f0c5eac780743a1a5d7b621fc1a5971f
Vasily Guzov <vasily.guzov@gmail.com>
2020-12-23 01:05:29 +0300

    Создал подменю для переключения между меню и отзывами

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,14 +1,27 @@
 import React, { useState, useMemo } from 'react';
 import Menu from './menu';
 import Navigation from './navigation';
+import Submenu from './submenu';
+import { SUB_MENU } from '../constants';
 
 export default function Restaurants(props) {
   const [activeId, setActiveId] = useState(props.restaurants[0].id);
+  const [activeMenu, setActiveMenu] = useState(SUB_MENU.MENU);
 
   const activeRestaurant = useMemo(
     () => props.restaurants.find((restaurant) => restaurant.id === activeId),
     [activeId, props.restaurants]
   );
+
+  const handleButtonSubmenu = (e) => {
+    const {
+      target: {
+        dataset: { menuitem },
+      },
+    } = e;
+
+    setActiveMenu(menuitem);
+  };
 
   return (
     <div>
@@ -16,7 +29,11 @@ export default function Restaurants(props) {
         restaurants={props.restaurants}
         onRestaurantClick={setActiveId}
       />
-      <Menu menu={activeRestaurant.menu} />
+
+      <Submenu handleBtn={handleButtonSubmenu} />
+
+      {activeMenu === SUB_MENU.MENU && <Menu menu={activeRestaurant.menu} />}
+      {activeMenu === SUB_MENU.REVIEWS && 'reviews'}
     </div>
   );
 }
