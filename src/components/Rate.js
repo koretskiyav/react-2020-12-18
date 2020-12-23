0e827c5f7e1a6c2c89e976dedcd234624e066022
DK <i386.net@gmail.com>
2020-12-23 10:35:57 +0300

    add Rate

---
+++ src/components/Rate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Rate = ({ rate }) => {
+  let stars = '';
+  if (rate === 0) {
+    stars = 'No one rated this place';
+  } else {
+    for (let i = 0; i < rate; i++) {
+      stars += '⭐️ ';
+    }
+  }
+
+  return (
+    <div>
+      <h3>Restaurant rating: {stars}</h3>
+    </div>
+  );
+};
+
+export default Rate;
--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,6 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import Menu from './menu';
 import Navigation from './navigation';
+import Rate from './Rate';
 
 export default function Restaurants(props) {
   const [activeId, setActiveId] = useState(props.restaurants[0].id);
@@ -10,12 +11,25 @@ export default function Restaurants(props) {
     [activeId, props.restaurants]
   );
 
+  const ratingHandler = () => {
+    if (activeRestaurant.reviews.length === 0) {
+      return 0;
+    }
+    const scores = activeRestaurant.reviews.map((review) => review.rating);
+    return (
+      scores.reduce((acc, current) => acc + current, 0) / scores.length
+    ).toFixed(0);
+  };
+
+  const rating = ratingHandler();
+
   return (
     <div>
       <Navigation
         restaurants={props.restaurants}
         onRestaurantClick={setActiveId}
       />
+      <Rate rate={rating} />
       <Menu menu={activeRestaurant.menu} />
     </div>
   );
