9aa6b577050e8d303fdc550c929210e02f4159c3
Vasily Guzov <vasily.guzov@gmail.com>
2020-12-26 01:13:01 +0300

    proptypes for the Review

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Rate from '../../rate';
 import styles from './review.module.css';
@@ -19,6 +20,11 @@ const Review = ({ user, text, rating }) => (
 
 Review.defaultProps = {
   user: 'Anonymous',
+};
+
+Review.propTypes = {
+  text: PropTypes.string,
+  rating: PropTypes.number,
 };
 
 export default Review;
