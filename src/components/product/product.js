1e187c86a1fb5bba2464b00c357c34eec39ccc51
koretskiyav <koretskiyav@gmail.com>
2020-12-25 21:23:41 +0200

    add Product tests

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './product.module.css';
 import MinusIcon from './icons/minus.svg';
@@ -6,30 +6,42 @@ import PlusIcon from './icons/plus.svg';
 
 import counter from '../../hocs/counter';
 
-const Product = ({ product, amount, increment, decrement }) => (
-  <div className={styles.product}>
-    <div className={styles.content}>
-      <div>
-        <h4 className={styles.title}>{product.name}</h4>
-        <p className={styles.description}>{product.ingredients.join(', ')}</p>
-        <div className={styles.price}>{product.price} $</div>
-      </div>
-      <div>
-        <div className={styles.counter}>
-          <div className={styles.count}>{amount}</div>
-          <div className={styles.buttons}>
-            <button className={styles.button} onClick={decrement}>
-              <img src={MinusIcon} alt="minus" />
-            </button>
-            <button className={styles.button} onClick={increment}>
-              <img src={PlusIcon} alt="plus" />
-            </button>
+const Product = ({ product, amount, increment, decrement, fetchData }) => {
+  useEffect(() => {
+    fetchData && fetchData(product.id);
+  }, []); // eslint-disable-line
+
+  return (
+    <div className={styles.product} data-id="product">
+      <div className={styles.content}>
+        <div>
+          <h4 className={styles.title}>{product.name}</h4>
+          <p className={styles.description}>{product.ingredients.join(', ')}</p>
+          <div className={styles.price}>{product.price} $</div>
+        </div>
+        <div>
+          <div className={styles.counter}>
+            <div className={styles.count} data-id="product-amount">
+              {amount}
+            </div>
+            <div className={styles.buttons}>
+              <button className={styles.button} onClick={decrement}>
+                <img src={MinusIcon} alt="minus" />
+              </button>
+              <button
+                className={styles.button}
+                onClick={increment}
+                data-id="product-increment"
+              >
+                <img src={PlusIcon} alt="plus" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Product.propTypes = {
   product: PropTypes.shape({
---
+++ src/components/product/product.test.js
@@ -0,0 +1,28 @@
+import Enzyme, { mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Product from './product';
+import { restaurants } from '../../fixtures';
+Enzyme.configure({ adapter: new Adapter() });
+
+const product = restaurants[0].menu[0];
+
+describe('Product', () => {
+  it('should render', () => {
+    const wrapper = mount(<Product product={product} />);
+    expect(wrapper.find('[data-id="product"]').length).toBe(1);
+  });
+  it('should init from 0 amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  });
+  it('should increment amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper.find('[data-id="product-increment"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+  });
+  it('should fetch data', () => {
+    const fn = jest.fn();
+    mount(<Product product={product} fetchData={fn} />);
+    expect(fn).toBeCalledWith(product.id);
+  });
+});
