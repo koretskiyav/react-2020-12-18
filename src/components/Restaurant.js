03be82cb9da5ed59223059382835ca498dd4735e
Pavel Zabolotny <p.zabolotny@bi.zone>
2020-12-25 18:53:57 +0300

    Average rating

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Rate from './Rate';
 import Menu from './menu';
 import Reviews from './Reviews';
+import style from './Restaurant.module.css';
 
 export default function Restaurant(props) {
+  let averageRating = () => {
+    let ratingLength = props.restaurant.reviews
+      ? props.restaurant.reviews.length
+      : 0;
+    let ratingCount = 0;
+    if (props.restaurant.reviews) {
+      for (const review of props.restaurant.reviews) {
+        ratingCount += review.rating;
+      }
+    }
+    return Math.round(ratingCount / ratingLength);
+  };
   return (
     <div>
+      <div className={style.rating}>
+        <div className={style.title}>Average rating:</div>
+        <Rate className={style.stars} rating={averageRating()} />
+      </div>
       <Menu menu={props.restaurant.menu} />
       <Reviews reviews={props.restaurant.reviews} />
     </div>
