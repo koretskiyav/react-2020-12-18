9572061c4bb9de154240be3f5a1121ac00c696c2
DK <i386.net@gmail.com>
2020-12-25 18:58:49 +0300

    update Restaurant:
    add useMemo for rating

--- src/components/Restaurants/Restaurant/Restaurant.js
+++ src/components/Restaurants/Restaurant/Restaurant.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Menu from '../Menu';
 import Reviews from '../Reviews/Reviews';
-import Rate from '../Rate';
+import Rating from '../../Rating/Rating';
 import styles from './Restaurant.module.css';
 
 const Restaurant = ({ restaurant }) => {
-  const ratingHandler = () => {
-    if (restaurant.reviews.length === 0) {
+  const { reviews, menu, name } = restaurant;
+
+  const rating = useMemo(() => {
+    if (reviews.length === 0) {
       return 0;
     }
-    const scores = restaurant.reviews.map((review) => review.rating);
-    return (
+    const scores = reviews.map((review) => review.rating);
+    return Math.floor(
       scores.reduce((acc, current) => acc + current, 0) / scores.length
-    ).toFixed(0);
-  };
-
-  const rating = ratingHandler();
+    );
+  }, [reviews]);
 
   return (
     <>
-      <h1 className={styles.restaurant__title}>{restaurant.name}</h1>
+      <h1 className={styles.restaurant__title}>{name}</h1>
       <div className={styles.restaurant}>
-        <Menu menu={restaurant.menu} />
+        <Menu menu={menu} />
         <div className={styles.restaurant__wrapper}>
-          <Reviews reviews={restaurant.reviews} />
-          <Rate rate={rating} />
+          <Reviews reviews={reviews} />
+          <h3>Average rating</h3>
+          <Rating rating={rating} />
         </div>
       </div>
     </>
