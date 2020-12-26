cec48634b74a86573709f223c8ec7234d6d5ee33
Vasily Guzov <vasily.guzov@gmail.com>
2020-12-27 00:16:15 +0300

    testing the decrement function, the solution without using the increment function

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -16,11 +16,9 @@ describe('Product', () => {
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
   it('should decrement amount', () => {
-    const wrapper = mount(<Product product={product} />);
+    const wrapper = mount(<Product product={product} amount={2} />);
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
-    wrapper.find('[data-id="product-decrement"]').simulate('click');
-    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
   });
   it('should fetch data', () => {
     const fn = jest.fn();
