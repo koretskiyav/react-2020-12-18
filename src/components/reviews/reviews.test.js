dbb17d9075cc02c811f7af4ac456d5247780ec62
DK <i386.net@gmail.com>
2020-12-28 14:49:01 +0300

    add reviews tests

---
+++ src/components/reviews/reviews.test.js
@@ -0,0 +1,21 @@
+import Enzyme, { mount } from 'enzyme';
+import Reviews from './reviews';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { restaurants } from '../../fixtures';
+Enzyme.configure({ adapter: new Adapter() });
+
+const reviews = restaurants[0].reviews;
+const estimatedReviewsAmount = reviews.length;
+
+describe('Reviews', () => {
+  it('should render', () => {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('[data-id="reviews"]').length).toBe(1);
+  });
+  it('should have review blocks', function () {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('[data-id="reviews"]').children().length).toBe(
+      estimatedReviewsAmount
+    );
+  });
+});
