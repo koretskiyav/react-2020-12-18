273504338d8ca179b973626468be891e204cb6b2
Vladimir Lapenkov <vlapenkov@ya.ru>
2020-12-28 11:53:14 +0300

    fix2

--- src/components/app.js
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from 'react';
-import Restaurants from './restaurants';
-
-export default class App extends PureComponent {
-  render() {
-    return <Restaurants restaurants={this.props.restaurants} />;
-  }
-}
