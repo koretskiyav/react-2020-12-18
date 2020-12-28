2bd2147dfd60aee115f2bec24afff1be42bf0c55
koretskiyav <koretskiyav@gmail.com>
2020-12-28 23:29:32 +0200

    HW 2.3 add Reviews tests

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -5,11 +5,15 @@ import Rate from '../../rate';
 import styles from './review.module.css';
 
 const Review = ({ user, text, rating }) => (
-  <div className={styles.review}>
+  <div className={styles.review} data-id="review">
     <div className={styles.content}>
       <div>
-        <h4 className={styles.name}>{user}</h4>
-        <p className={styles.comment}>{text}</p>
+        <h4 className={styles.name} data-id="review-user">
+          {user}
+        </h4>
+        <p className={styles.comment} data-id="review-text">
+          {text}
+        </p>
       </div>
       <div className={styles.rate}>
         <Rate value={rating} />
