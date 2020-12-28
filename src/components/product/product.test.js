43229d3c79e262d8ce66ed3d4b01992c374175d5
Vasily Guzov <vasily.guzov@gmail.com>
2020-12-28 21:01:03 +0300

    fix fake tests

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -16,9 +16,11 @@ describe('Product', () => {
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
   it('should decrement amount', () => {
-    const wrapper = mount(<Product product={product} amount={2} />);
+    const wrapper = mount(<Product product={product} />);
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
   });
   it('should fetch data', () => {
     const fn = jest.fn();
