f4c3b46b3ff126b54c477daed55cfc34da52be24
tatiyanakryzhnyaya <kryzhnyayatatiyana@gmail.com>
2020-12-28 23:23:20 +0300

    HT2 add proptTypes to components

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Rate from '../../rate';
 import styles from './review.module.css';
+import PropTypes from 'prop-types';
 
 const Review = ({ user, text, rating }) => (
   <div className={styles.review}>
@@ -19,6 +20,14 @@ const Review = ({ user, text, rating }) => (
 
 Review.defaultProps = {
   user: 'Anonymous',
+  text: '',
+  rating: 0,
+};
+
+Review.propTypes = {
+  user: PropTypes.string,
+  text: PropTypes.string,
+  rating: PropTypes.number,
 };
 
 export default Review;
