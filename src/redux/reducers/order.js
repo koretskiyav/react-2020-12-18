ef5c8259f5bfe2c97b79888382b5309d97f61795
Marina Balioura <mettta@gmail.com>
2020-12-31 00:29:14 +0100

    HT3: add REMOVE action

--- src/redux/reducers/order.js
+++ src/redux/reducers/order.js
@@ -1,4 +1,4 @@
-import { DECREMENT, INCREMENT } from '../constants';
+import { DECREMENT, INCREMENT, REMOVE } from '../constants';
 
 // { [productId]: amount }
 export default (state = 0, action) => {
@@ -8,6 +8,10 @@ export default (state = 0, action) => {
       return { ...state, [payload.id]: (state[payload.id] || 0) + 1 };
     case DECREMENT:
       return { ...state, [payload.id]: (state[payload.id] || 0) - 1 };
+    case REMOVE:
+      const newState = { ...state };
+      delete newState[payload.id];
+      return newState;
     default:
       return state;
   }
