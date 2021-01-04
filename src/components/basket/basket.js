59ef1456882e30b08f46f697f673b0295d11f00c
tatiyanakryzhnyaya <kryzhnyayatatiyana@gmail.com>
2021-01-05 02:08:03 +0300

    add minimal style

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { clear, increment, decrement } from '../../redux/actions';
@@ -14,43 +14,57 @@ const Basket = ({
   increment,
   decrement,
 }) => {
+  const [isOpenBasket, toggleBasket] = useState(false);
+  const hideBasket = () => toggleBasket(false);
+  const openBasket = () => toggleBasket(true);
   const products = useMemo(() => {
     return restaurants.flatMap(({ menu }) => menu);
   }, [restaurants]);
 
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id} className={styles.basket}>
-          {product.name}
-          <div>Price {product.price} $ </div>
-          <div> Count {order[product.id] || 0}</div>
-          <div>Total coast {order[product.id] * product.price || 0}</div>
-          <div className={styles.buttons}>
-            <button
-              className={styles.button}
-              onClick={() => decrement(product.id)}
-              data-id="product-decrement"
-            >
-              <img src={MinusIcon} alt="minus" />
-            </button>
-            <button
-              className={styles.button}
-              onClick={() => increment(product.id)}
-              data-id="product-increment"
-            >
-              <img src={PlusIcon} alt="plus" />
-            </button>
-          </div>
-        </div>
-      ))}
+    <div className={styles.basket}>
       <button
         className={styles.button}
-        onClick={clearBasket}
+        onClick={isOpenBasket ? hideBasket : openBasket}
         data-id="basket-clear"
       >
-        x
+        ≡
       </button>
+      {isOpenBasket && (
+        <div className={styles.products}>
+          {products.map((product) => (
+            <div key={product.id} className={styles.product}>
+              {product.name}
+              <div>Price {product.price} $ </div>
+              <div> Count {order[product.id] || 0}</div>
+              <div>Total coast {order[product.id] * product.price || 0}</div>
+              <div className={styles.buttons}>
+                <button
+                  className={styles.button}
+                  onClick={() => decrement(product.id)}
+                  data-id="product-decrement"
+                >
+                  <img src={MinusIcon} alt="minus" />
+                </button>
+                <button
+                  className={styles.button}
+                  onClick={() => increment(product.id)}
+                  data-id="product-increment"
+                >
+                  <img src={PlusIcon} alt="plus" />
+                </button>
+              </div>
+            </div>
+          ))}
+          <button
+            className={styles.button}
+            onClick={clearBasket}
+            data-id="basket-clear"
+          >
+            x
+          </button>
+        </div>
+      )}
     </div>
   );
 };
