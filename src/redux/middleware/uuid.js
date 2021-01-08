3dbdaee55381d0edf909ddf9b7bd89605fedbcb3
Marina Balioura <mettta@gmail.com>
2021-01-08 05:27:47 +0100

    HT4: uuid.js: clean up

--- src/redux/middleware/uuid.js
+++ src/redux/middleware/uuid.js
@@ -1,12 +1,12 @@
 
 import { v4 as uuidv4 } from 'uuid';
 
-export default (store) => (next) => (action) => {
+export default () => (next) => (action) => {
 
   if (action.type === 'ADD_REVIEW' || action.type === 'ADD_USER') {
     const uuid = uuidv4();
     action.payload = {
-      id: ('00-' + uuid),
+      id: (uuid),
       ...action.payload
     }
   }
