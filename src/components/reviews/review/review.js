68bc9c7220123412d2e762529269ea1820a292f4
Vladimir Lapenkov <vlapenkov@ya.ru>
2021-01-09 12:47:21 +0300

    users added

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -5,14 +5,15 @@ import Rate from '../../rate';
 import styles from './review.module.css';
 import { connect } from 'react-redux';
 
-const Review = ({ review }) => {
+const Review = ({ review, user }) => {
   console.log(review);
   return (
     <div className={styles.review} data-id="review">
       <div className={styles.content}>
         <div>
           <h4 className={styles.name} data-id="review-user">
-            {review.user}
+            {user.name}
+            <pre>{user.id}</pre>
           </h4>
           <p className={styles.comment} data-id="review-text">
             {review.text}
@@ -37,7 +38,10 @@ Review.defaultProps = {
   user: 'Anonymous',
 };
 
-export default connect((state, ownProps) => ({
-  review: state.reviews[ownProps.id],
-}))(Review);
+export default connect((state, ownProps) => {
+  const review = state.reviews[ownProps.id];
+  const user = state.users[review.userId];
+
+  return { review, user };
+})(Review);
 //export default Review;
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,10 +3,12 @@ import order from './order';
 import restaurants from './restaurants';
 import products from './products';
 import reviews from './reviews';
+import users from './users';
 
 export default combineReducers({
   order,
   restaurants,
   products,
   reviews,
+  users,
 });
