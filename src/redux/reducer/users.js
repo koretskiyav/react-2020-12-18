502eee9874dcc16b6e41f6583ee4a68a3a583eab
Vasily Guzov <vasily.guzov@gmail.com>
2021-01-11 23:56:21 +0300

    add reduser Users normalized data

---
+++ src/redux/reducer/users.js
@@ -0,0 +1,15 @@
+import { normalizedReviews } from '../../fixtures';
+
+const defaultReviews = normalizedReviews.reduce(
+  (acc, review) => ({ ...acc, [review.id]: review }),
+  {}
+);
+
+export default (reviews = defaultReviews, action) => {
+  const { type } = action;
+
+  switch (type) {
+    default:
+      return reviews;
+  }
+};
