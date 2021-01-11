5c9f65c054381cf0cd0bdcd88e22ac72be68a099
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-01-11 12:37:20 +0200

    HT4: add middleware, that push uuid to values

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,9 @@
-import { INCREMENT, DECREMENT, REMOVE } from './constants';
+import { INCREMENT, DECREMENT, REMOVE, ADD_REVIEW } from './constants';
 
 export const increment = (id) => ({ type: INCREMENT, payload: { id } });
 export const decrement = (id) => ({ type: DECREMENT, payload: { id } });
 export const remove = (id) => ({ type: REMOVE, payload: { id } });
+export const addReview = (values) => ({
+  type: ADD_REVIEW,
+  payload: { values },
+});
---
+++ src/redux/middleware/uuidCreator.js
@@ -0,0 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default (store) => (next) => (action) => {
+  const uuid = uuidv4();
+
+  action.payload.values = { id: uuid, ...action.payload.values };
+  console.log(action.payload.values);
+
+  next(action);
+};
