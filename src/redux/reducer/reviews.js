e2ac294429024d091f29706f65e02da23be72ccd
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-01-11 13:05:12 +0200

    HT4: add new reviews to the store

--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,4 +1,6 @@
 import { normalizedReviews } from '../../fixtures';
+import { ADD_REVIEW } from '../constants';
+import { reviewsSelector } from '../selectors';
 
 const defaultReviews = normalizedReviews.reduce(
   (acc, review) => ({ ...acc, [review.id]: review }),
@@ -6,9 +8,13 @@ const defaultReviews = normalizedReviews.reduce(
 );
 
 export default (reviews = defaultReviews, action) => {
-  const { type } = action;
+  const { type, payload } = action;
 
   switch (type) {
+    case ADD_REVIEW:
+      const addedNewReview = payload.values;
+
+      return { ...reviews, [addedNewReview.id]: addedNewReview };
     default:
       return reviews;
   }
