c5194e2a955ee011c2816fa7a990a133f10ee950
koretskiyav <koretskiyav@gmail.com>
2021-01-15 21:02:19 +0200

    add Not found page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Restaurants from '../restaurants';
 import Header from '../header';
 import Basket from '../basket';
@@ -9,8 +9,12 @@ export default class App extends PureComponent {
     return (
       <div>
         <Header />
-        <Route path="/checkout" component={Basket} />
-        <Route path="/restaurants/:restId" component={Restaurants} />
+        <Switch>
+          <Route path="/" exact component={() => 'Main page'} />
+          <Route path="/checkout" component={Basket} />
+          <Route path="/restaurants/:restId" component={Restaurants} />
+          <Route path="/" component={() => '404 - Not found'} />
+        </Switch>
       </div>
     );
   }
