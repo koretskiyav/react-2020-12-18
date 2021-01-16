ecf473c2a95348902ce87b5066240a562ce938fb
Marina Balioura <mettta@gmail.com>
2021-01-16 15:20:40 +0100

    HT6: restaurant.js: replacing Tabs with Route

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -2,30 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { createStructuredSelector } from 'reselect';
+import { Route, Switch, Link } from 'react-router-dom';
 
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
-import Tabs from '../tabs';
+// import Tabs from '../tabs';
 import { averageRatingSelector } from '../../redux/selectors';
 
 const Restaurant = ({ restaurant, averageRating }) => {
   const { id, name, menu, reviews } = restaurant;
-  const tabs = [
-    { title: 'Menu', content: <Menu menu={menu} restaurantId={id} /> },
-    {
-      title: 'Reviews',
-      content: <Reviews reviews={reviews} restaurantId={id} />,
-    },
-  ];
+  // const tabs = [
+  //   { title: 'Menu', content: <Menu menu={menu} restaurantId={id} /> },
+  //   {
+  //     title: 'Reviews',
+  //     content: <Reviews reviews={reviews} restaurantId={id} />,
+  //   },
+  // ];
 
   return (
     <div>
       <Banner heading={name}>
         {!!averageRating && <Rate value={averageRating} />}
       </Banner>
-      <Tabs tabs={tabs} />
+      {/* <Tabs tabs={tabs} /> */}
+
+      <Link to={`/restaurants/${id}`}>Menu</Link>
+      <Link to={`/restaurants/${id}/reviews`}>Reviews</Link>
+      <Switch>
+        <Route path={`/restaurants/${id}/reviews`}>
+          <Reviews reviews={reviews} restaurantId={id} />
+        </Route>
+        <Route path={`/restaurants/${id}`}>
+          <Menu menu={menu} restaurantId={id} />
+        </Route>
+      </Switch>
     </div>
   );
 };
