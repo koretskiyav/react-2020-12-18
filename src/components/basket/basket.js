02a5565780e962bc08c25277470117a05ed46d03
koretskiyav <koretskiyav@gmail.com>
2021-01-19 21:04:01 +0200

    add react-transition-group

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+import './basket.css';
 import styles from './basket.module.css';
+
 import itemStyles from './basket-item/basket-item.module.css';
 import BasketItem from './basket-item';
 import Button from '../button';
@@ -27,15 +30,22 @@ function Basket({ title = 'Basket', total, orderProducts }) {
       <h4 className={styles.title}>
         <UserConsumer>{({ name }) => `${name}'s ${title}`}</UserConsumer>
       </h4>
-      {orderProducts.map(({ product, amount, subtotal, restaurantId }) => (
-        <BasketItem
-          product={product}
-          amount={amount}
-          key={product.id}
-          subtotal={subtotal}
-          restaurantId={restaurantId}
-        />
-      ))}
+      <TransitionGroup>
+        {orderProducts.map(({ product, amount, subtotal, restaurantId }) => (
+          <CSSTransition
+            key={product.id}
+            timeout={10000}
+            classNames="basket-animation"
+          >
+            <BasketItem
+              product={product}
+              amount={amount}
+              subtotal={subtotal}
+              restaurantId={restaurantId}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
       <hr className={styles.hr} />
       <div className={itemStyles.basketItem}>
         <div className={itemStyles.name}>
