f4f7b965cd0a1f311bce4a6ce7df835fbf8923b9
Ilya Edelshtein <ilaltim@gmail.com>
2021-01-19 22:38:25 +0200

    init

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+import './basket.css';
 import styles from './basket.module.css';
+
 import itemStyles from './basket-item/basket-item.module.css';
 import BasketItem from './basket-item';
 import Button from '../button';
 import { orderProductsSelector, totalSelector } from '../../redux/selectors';
+import { UserConsumer } from '../../contexts/user-context';
 
 function Basket({ title = 'Basket', total, orderProducts }) {
+  // const { name } = useContext(userContext);
+
   if (!total) {
     return (
       <div className={styles.basket}>
@@ -20,15 +26,26 @@ function Basket({ title = 'Basket', total, orderProducts }) {
 
   return (
     <div className={styles.basket}>
-      <h4 className={styles.title}>{title}</h4>
-      {orderProducts.map(({ product, amount, subtotal }) => (
-        <BasketItem
-          product={product}
-          amount={amount}
-          key={product.id}
-          subtotal={subtotal}
-        />
-      ))}
+      {/* <h4 className={styles.title}>{`${name}'s ${title}`}</h4> */}
+      <h4 className={styles.title}>
+        <UserConsumer>{({ name }) => `${name}'s ${title}`}</UserConsumer>
+      </h4>
+      <TransitionGroup>
+        {orderProducts.map(({ product, amount, subtotal, restaurantId }) => (
+          <CSSTransition
+            key={product.id}
+            timeout={10000}
+            classNames="basket-animation"
+          >
+            <BasketItem
+              product={product}
+              amount={amount}
+              subtotal={subtotal}
+              restaurantId={restaurantId}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
       <hr className={styles.hr} />
       <div className={itemStyles.basketItem}>
         <div className={itemStyles.name}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,21 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { routerMiddleware } from 'connected-react-router';
 import reducer from './reducer';
 
 import logger from './middleware/logger';
 import generateId from './middleware/generateId';
 import api from './middleware/api';
 
-const enhancer = applyMiddleware(thunk, api, generateId, logger);
+import history from '../history';
+
+const enhancer = applyMiddleware(
+  thunk,
+  api,
+  generateId,
+  routerMiddleware(history),
+  logger
+);
 
 export default createStore(reducer, composeWithDevTools(enhancer));
