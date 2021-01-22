5622f54e5599a23107771fb9f19ed901e1895b1f
koretskiyav <koretskiyav@gmail.com>
2021-01-22 13:40:04 +0200

    add HT7.4

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import Review from './review';
 import ReviewForm from './review-form';
 import styles from './reviews.module.css';
+import animationStyles from './reviews-animation.module.css';
 
 import { loadReviews, loadUsers } from '../../redux/actions';
 import {
@@ -31,9 +33,13 @@ const Reviews = ({
 
   return (
     <div className={styles.reviews}>
-      {reviews.map((id) => (
-        <Review key={id} id={id} />
-      ))}
+      <TransitionGroup>
+        {reviews.map((id) => (
+          <CSSTransition key={id} timeout={500} classNames={animationStyles}>
+            <Review id={id} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
       <ReviewForm restaurantId={restaurantId} />
     </div>
   );
